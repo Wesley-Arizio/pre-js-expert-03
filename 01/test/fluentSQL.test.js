b3e04f62b6cd8a0b165cf55fadb110b7c6cac741
wesley_arizio <wesleyarizio26@gmail.com>
2021-03-19 00:22:37 -0300

    setting up first class project

---
+++ 01/test/fluentSQL.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "@jest/globals";
+
+describe("Test Suit for fluentSQL builder", () => {
+  it("should sum two numbers", () => {
+    const sum = (a, b) => a + b;
+    expect(sum(1, 1)).toBe(2);
+  });
+});

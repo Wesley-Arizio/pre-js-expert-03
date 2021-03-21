b4346209ec808b590321d10b2e17550a03bbcc57
wesley_arizio <wesleyarizio26@gmail.com>
2021-03-21 03:56:01 -0300

    adding fluentSQL unit tests

--- 01/test/fluentSQL.test.js
+++ 01/test/fluentSQL.test.js
@@ -1,8 +1,84 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, test } from "@jest/globals";
+import FluentSQLBuilder from "../src/fluentSQL";
+
+import data from "./mock/data.json";
+
+const mocks = {
+  data,
+};
 
 describe("Test Suit for fluentSQL builder", () => {
-  it("should sum two numbers", () => {
-    const sum = (a, b) => a + b;
-    expect(sum(1, 1)).toBe(2);
+  it("#for should return a FluentSQLBuilder instance", () => {
+    const fluentBuilder = FluentSQLBuilder.for(mocks.data);
+
+    expect(fluentBuilder).toBeInstanceOf(FluentSQLBuilder);
+  });
+
+  it("#build should return the empty object instance", () => {
+    const fluentBuilder = FluentSQLBuilder.for(mocks.data).build();
+    const expected = mocks.data;
+
+    expect(fluentBuilder).toStrictEqual(expected);
+  });
+
+  it("#limit given a collection it should limit results", () => {
+    const fluentBuilder = FluentSQLBuilder.for(mocks.data).limit(1).build();
+    const [expected] = mocks.data;
+
+    expect(fluentBuilder).toStrictEqual([expected]);
+  });
+
+  it("#where given a collection it should filter data", () => {
+    const fluentBuilder = FluentSQLBuilder.for(mocks.data)
+      .where({
+        category: /^dev/,
+      })
+      .build();
+
+    const expected = mocks.data.filter(
+      ({ category }) => category.slice(0, 3) === "dev"
+    );
+
+    expect(fluentBuilder).toStrictEqual(expected);
+  });
+
+  it("#select given a collection it should return only specific fields", () => {
+    const fluentBuilder = FluentSQLBuilder.for(mocks.data)
+      .select(["name", "category"])
+      .build();
+
+    const expected = mocks.data.map(({ name, category }) => ({
+      name,
+      category,
+    }));
+
+    expect(fluentBuilder).toStrictEqual(expected);
+  });
+
+  it("#orderBy given a collection it should order results by field", () => {
+    const fluentBuilder = FluentSQLBuilder.for(mocks.data)
+      .orderBy("name")
+      .build();
+
+    const [first, second, third] = mocks.data;
+
+    const expected = [first, third, second];
+
+    expect(fluentBuilder).toStrictEqual(expected);
+  });
+
+  it("pipeline", () => {
+    const fluentBuilder = FluentSQLBuilder.for(mocks.data)
+      .where({ category: "dedveloper" })
+      .where({ name: /^M/ })
+      .select(["name", "category"])
+      .orderBy("name")
+      .build();
+
+    const [_, expected] = mocks.data;
+
+    delete expected.id;
+
+    expect(fluentBuilder).toStrictEqual([expected]);
   });
 });
